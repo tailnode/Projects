3fc16615c96bd348c95ae879aad817f4b0ceb589
tailnode <tailnode@gmail.com>
2013-11-24 13:22:57 +0800

    change tab to 4 spaces

--- Numbers/fibonacci.py
+++ Numbers/fibonacci.py
@@ -2,12 +2,12 @@
 
 
 def generate_fibonacci(n):
-	fibonacci = [1]
-	for i in range(n - 1):
-		fibonacci.append(sum(fibonacci[-2:]))
-	return fibonacci if n > 0 else []
+    fibonacci = [1]
+    for i in range(n - 1):
+        fibonacci.append(sum(fibonacci[-2:]))
+    return fibonacci if n > 0 else []
 
 if __name__ == '__main__':
-	for i in range(10):
-		print(generate_fibonacci(i))
-		print('\n')
+    for i in range(10):
+        print(generate_fibonacci(i))
+        print('\n')
