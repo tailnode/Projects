ea72c48b1ef45638ed1a671e1bba95aa5e891f61
tailnode <tailnode@gmail.com>
2013-12-07 19:14:33 +0800

    done. commit
    Signed-off-by: tailnode <tailnode@gmail.com>

--- Numbers/alarm_clock.py
+++ Numbers/alarm_clock.py
@@ -2,24 +2,37 @@
 
 import threading
 import os
+from time import mktime
+from time import localtime
 
 
+
+def alarm():
+    alarm_file = 'alarm.wav'
+    os.popen('aplay -q ' + alarm_file)
 
 def set_timer(time):
-    alarm_file = 'alarm.wav'
-    def alarm():
-        os.popen('aplay -q ' + alarm_file)
-
     t = threading.Timer(time, alarm)
     t.start()
 
-time_input = input('enter time(min.sec)\n')
+output_string = {'1':'after', '2':'at'}
 try:
+    mode = input('enter mode\n1.alarm after input time\n2.alarm at input time\n')
+    if (mode not in ['1', '2']) : raise
+
+    time_input = input('enter time(hour.min.sec)\n')
     time  = time_input.split('.')
-    if time.length() != 2 : raise
-    print('alarm after', time[0], 'minite and', time[1], 'second')
-    set_timer(int(time[0]) * 60 + int(time[1]))
+    if len(time) != 3 : raise
+
+    print('alarm', output_string[mode], time[0], 'hour', time[1], 'minite and', time[2], 'second')
+    if mode == '1':
+        set_timer(int(time[0]) * 3600 + int(time[1]) * 60 + int(time[2]))
+    else:
+        time_alarm = list(localtime())
+        for i in range(3):
+            time_alarm[i+3] = int(time[i])
+        seconds = mktime(tuple(time_alarm)) - mktime(localtime())
+        if seconds <= 0 : raise
+        set_timer(seconds)
 except:
     print('input error.')
-
-
